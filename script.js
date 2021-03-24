cd09bd432ad675adb9b96892af9d41ac9ae4f425
Sarthak Jain <sarthakjain143341@gmail.com>
2021-03-25 00:08:06 +0530

    Dynamically Movies Lists generate on Search

--- script.js
+++ script.js
@@ -9,20 +9,27 @@ window.onload= function (){
 	});
 }
 
-var obj = {
-	"Bad Boys for Life": 38700,
-	"Godzilla: King of the Monsters": 373571,
-	"Joker": 475557,
-	"Cherry": 544401,
-	"Red Dot": 649087,
-	"Breach": 651571,
-	"The Little Things": 602269,
-	"Outside the Wire": 775996,
-	"Black Water: Abyss": 522444,
-	"Monster Hunter": 458576
-};
-
 function loadDoc() 
+{
+	document.getElementById("inner_div").innerHTML = "";
+	var input = document.getElementById("inputBox").value;
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) 
+		{
+			var myObj = JSON.parse(this.responseText);
+			for(var i = 0; i < myObj.results.length; i++)
+			{
+				addDivision(myObj.results[i].poster_path, myObj.results[i].title, myObj.results[i].overview);
+			}
+		}
+	};
+	xhttp.open("GET", "https://api.themoviedb.org/3/search/movie?api_key=8fe364fa223db9a3f9720860af5fdd5d&query=" + input, true);
+	xhttp.send();
+}
+
+function addDivision(image_path, movie_title, movie_brief)
 {
 	var input = document.getElementById("inputBox").value;
 	if(input !== "" && k === 1)
@@ -32,8 +39,6 @@ function loadDoc()
 	}
 	document.getElementById("inputBox").value = "";
 
-	var xhttp = new XMLHttpRequest();
-
 	var img = document.createElement('img');
 
 	var h2 = document.createElement("h2");
@@ -41,26 +46,24 @@ function loadDoc()
 	var division = document.createElement("div");
 	division.setAttribute("id", "inner_div_" + k++);
 
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) 
-		{
-			var myObj = JSON.parse(this.responseText);
-			img.src = "https://www.themoviedb.org/t/p/original//" + myObj.poster_path;
-			img.style.height = "250px";
-			img.style.width = "200px";
+	if(image_path !== null)
+	{
+		img.src = "https://www.themoviedb.org/t/p/original//" + image_path;
+	}
+	else
+	{
+		img.src = "E:\\JS Practise\\API Integration\\Default.png"
+	}
+	img.style.height = "250px";
+	img.style.width = "200px";
 
-			division.appendChild(img);
-
-			var textNode = document.createTextNode(myObj.original_title);
-			h2.appendChild(textNode);
-			division.appendChild(h2);
-			var paragraph = document.createElement("p");
-			var textNode1 = document.createTextNode(myObj.overview);
-			paragraph.appendChild(textNode1);
-			division.appendChild(paragraph);
-			document.getElementById("inner_div").appendChild(division);
-		}
-	};
-	xhttp.open("GET", "https://api.themoviedb.org/3/movie/" + obj[input] + "?api_key=8fe364fa223db9a3f9720860af5fdd5d", true);
-	xhttp.send();
+	division.appendChild(img);
+	var textNode = document.createTextNode(movie_title);
+	h2.appendChild(textNode);
+	division.appendChild(h2);
+	var textNode1 = document.createTextNode(movie_brief);
+	var paragraph = document.createElement("p");
+	paragraph.appendChild(textNode1);
+	division.appendChild(paragraph);
+	document.getElementById("inner_div").appendChild(division);
 }
